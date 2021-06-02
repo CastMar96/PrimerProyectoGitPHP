ff94ca57aec7f7dcae0e09c3b2b17013206c4890
CastMar96 <castellanosm3009@gmail.com>
2021-06-01 19:19:13 -0600

    First Version

---
+++ pagina1.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+ <?php
+ echo "Hola mundo Andrea Castellanos";
+ ?>
+</body>
+</html>
---
+++ problema.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Problema propuesto</title>
+</head>
+<body>
+    <?php 
+    echo "Hola mi nombre es Andrea. <br>";
+    echo 'Tengo 24 años.<br>';
+    echo 'Estamos iniciando el curso de aprendizaje de PHP.<br>';
+    ?>
+</body>
+</html>
